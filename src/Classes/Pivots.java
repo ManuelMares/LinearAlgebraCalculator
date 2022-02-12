e27de80e7e5e99127932691fc0efee8a6fd05a4c
ManuelMares <fmms@outlook.com>
2022-02-12 15:42:42 -0700

    Classes have been added

---
+++ src/Classes/Pivots.java
@@ -0,0 +1,181 @@
+package Classes;
+
+public class Pivots{
+    public String[] names;
+    public int[][] positions;
+    public double[] values;
+    public boolean[] areFree;
+    public int size;
+
+    public Pivots(){
+        names = new String[0];
+        positions = new int[0][0];
+        values = new double[0];
+        areFree = new boolean[0];
+        size = 0;
+    }
+
+    public void SetPivotsToZero(){
+        names = new String[0];
+        positions = new int[0][0];
+        values = new double[0];
+        areFree = new boolean[0];
+        size = 0;
+    }
+
+    public void AddFreeVariable(String name){
+        int[] position = {-1, -1};        
+        AddPivot(name, position, 0, true);
+    }
+
+    public void AddPivot(String name, int[] position, double value, boolean isFree){
+        String[] newNames = new String[1];
+        int[][] newPositions = new int[1][2];
+        double[] newValues = new double[1];
+        boolean[] newAreFree = new boolean[1];
+        int counter = 0;
+
+        //this if is equal if(names != null && names.length > 1) = if(size > 0)
+        boolean isnull = (names == null) ? true : false; 
+        int length = names.length;
+        if(names != null && names.length > 0 && size > 0){
+            newNames = new String[names.length + 1];
+            newPositions = new int[positions.length + 1][2];
+            newValues = new double[values.length + 1];
+            newAreFree = new boolean[areFree.length + 1];
+            for (int index = 0; index < size; index++) {
+                newNames[counter] = names[counter];
+                newPositions[counter][0] = positions[counter][0];
+                newPositions[counter][1] = positions[counter][1];
+                newValues[counter] = values[counter];
+                newAreFree[counter] = areFree[counter];
+                counter++;
+            }
+        }
+
+        newNames[counter] = name;
+        newPositions[counter][0] = position[0];
+        newPositions[counter][1] = position[1];
+        newValues[counter] = value;
+        newAreFree[counter] = isFree;
+
+        names = newNames;
+        positions = newPositions;
+        values = newValues;
+        areFree = newAreFree;
+
+        size++;
+    }
+    
+    public void DeletePivot(int indexDelete){
+        String[] newNames = new String[names.length - 1];
+        int[][] newPositions = new int[positions.length - 1][2];
+        double[] newValues = new double[values.length - 1];
+        boolean[] newAreFree = new boolean[areFree.length - 1];
+
+        int counter = 0;
+        for (int index = 0; index < newNames.length; index++) {
+            if(index != indexDelete){
+                newNames[counter] = names[counter];
+                newPositions[counter][0] = positions[counter][0];
+                newPositions[counter][1] = positions[counter][1];
+                newValues[counter] = values[counter];
+                newAreFree[counter] = areFree[counter];
+                counter++;
+            }
+        }
+
+        names = newNames;
+        positions = newPositions;
+        values = newValues;
+        areFree = newAreFree;
+
+        size--;
+    }
+
+    public void UpdatePivot_Name(){}
+    public void UpdatePivot_Position(){}
+    public void UpdatePivot_Value(){}
+    public void UpdatePivot_isFree(){}
+
+    
+    public String GetPivot_Name(String name){
+        for (int index = 0; index < size; index++) {
+            if(names[index].equals(name)){
+                return names[index];
+            }
+        }
+        return " ";
+    }
+    public int[] GetPivot_Position(String name){
+        int[] position = {-1, -1};
+        for (int index = 0; index < size; index++) {
+            if(names[index].equals(name)){
+                position = positions[index];
+                return position;
+            }
+        }
+        return position;
+    }
+    public double GetPivot_Value(String name){
+        double value = 0;
+        for (int index = 0; index < size; index++) {
+            if(names[index].equals(name)){
+                value = values[index];
+                return value;
+            }
+        }
+        return value;
+    }
+    public boolean GetPivot_isFree(String name){
+        boolean isFree = false;
+        for (int index = 0; index < size; index++) {
+            if(names[index].equals(name)){
+                isFree = areFree[index];
+                return isFree;
+            }
+        }
+        return isFree;
+    }
+
+    public boolean Validate_ColumnHasPivot(int indexColumn){
+        //If there is any element in positions whose value [1] (columns) is equal to the parameter, then that column has already a pivot
+        for (int[] position: positions) {
+            if(position[1] == indexColumn)
+                return true;
+        }
+        return false;
+    }
+    
+    public boolean Validate_RowHasPivot(int indexRow){
+        //If there is any element in positions whose value [0] (rows) is equal to the parameter, then that row has already a pivot
+        for (int[] position: positions) {
+            if(position[0] == indexRow)
+                return true;
+        }
+        return false;
+    }
+
+    public void PrintPivots(){
+        String topDivisor = "------------";
+        System.out.println("\n\nDetail report of variables' status....");
+        System.out.printf("|%-12s|%-12s|%-12s|%-12s|\n", topDivisor, topDivisor, topDivisor, topDivisor);
+        System.out.printf("|%-12s|%-12s|%-12s|%-12s|\n", "Name", "Position", "Values", "Status");
+        System.out.printf("|%-12s|%-12s|%-12s|%-12s|\n", topDivisor, topDivisor, topDivisor, topDivisor);
+        for (int index = 0; index < names.length; index++) {
+            if(areFree[index]){
+                String name =names[index];
+                String na ="N/A";
+                String status = "Free";
+                System.out.printf("|%-12s|%-12s|%-12s|%-12s|\n", name, na, na, status);
+            }else{
+                String name =names[index];
+                String position = "(" + (positions[index][0]+1) +","+ (positions[index][1]+1) + ")";
+                String status = "Basic";
+                System.out.printf("|%-12s|%-12s|%-12.2f|%-12s|\n", name, position, values[index], status);
+            }
+        }        
+        System.out.printf("|%-12s|%-12s|%-12s|%-12s|\n\n\n", topDivisor, topDivisor, topDivisor, topDivisor);
+    }
+
+}
--- src/Methods/Matrix/SolveMatrix/SolveMatrix_ReduceEchelon.java
+++ src/Methods/Matrix/SolveMatrix/SolveMatrix_ReduceEchelon.java
@@ -1,262 +1,111 @@
 package Methods.Matrix.SolveMatrix;
 
-import Shared.MatrixMethods;
-import Shared.Validations;
+import Classes.Matrix;
+import Classes.Validations;
 import Variables.Dictionary;
 
 public class SolveMatrix_ReduceEchelon {
     //IMPORTS
     static Validations validation = new Validations();
-    static MatrixMethods matrixMethods = new MatrixMethods();
+    static Matrix matrix = new Matrix("Matrix A");
 
     public void Main(){
-        double[][] matrix = getValidMatrix();
-        
-        System.out.println("------------SOLUTION STEP BY STEP:------------"); 
-        double[][] solvedMatrix = Reduce_StepByStep(matrix); 
-     
-        
-        System.out.println("\n\n------------CONCLUSION:------------\n"); 
-        int amountPivots = matrixMethods.AugmentedMatrix_GetMaxNumberPivots(matrix.length, matrix[0].length);    
-        Dictionary variablesValues = GetVariablesValues(solvedMatrix, amountPivots);
-        PrintConclusions(solvedMatrix, amountPivots, variablesValues);
+        System.out.println("\n\n\nLet's start by indicate the size of the matrix");
+        CreateNewMatrix();
+        PrepareMatrix();
+        Reduce_GaussJordan();
+        System.out.println("======================== Conclusion ========================");
+        PrintConclusion();
+
+        //iteration try for pivots
     }
-    
-    private void PrintConclusions(double[][] matrix, int amountPivots, Dictionary variablesValues) {
-        //There are 4 options
-        //1. Extra columns -->  free variables
-        //2. Row with 0=0 --> Ignore it
-        //3. Row with 0=cte --> Inconsisten system
-        //4. all columns and rows have non zeros coeficients and values, then that is the unique solution
 
-        //the third case has to be checked in every alternative, since it is independet of the amount of pivots or size of the matrix.
+    private void PrintConclusion(){
+        matrix.PrintMatrix("\nReduce Echelon matrix:");
+        int inconsistentRow = matrix.CheckConsistentsy();
+        boolean isConsistent = (inconsistentRow == 0) ? true: false;
+        boolean isLI = matrix.IsLinearlyIndependent();
 
-        //THE SYSTEM CAN HAVE EXTRA ROWS OR EXTRA COLUMNS, BUT NOT BOTH.
-        
-        int index_InconsistentRow = CheckConsistency(matrix);
-        if(index_InconsistentRow >=0){
-            System.out.print("A equation of the form 0=cte has been located in the matrix (row "+(index_InconsistentRow+1)+")."); 
-            System.out.println("Hence the whole system is: \nINCONSISTNTE\n\n");
-        }else{
-            int[] freeVariables_Pivots = ListPivots_AreFreeVariables(matrix, variablesValues);
-            if(freeVariables_Pivots != null && freeVariables_Pivots.length > 0){
-                int basicVariables_Reduced = amountPivots - freeVariables_Pivots.length;
-                PrintVariablesValues(matrix, basicVariables_Reduced, freeVariables_Pivots);
-                PrintFreVariables(matrix, basicVariables_Reduced, freeVariables_Pivots);
+        //if consistent, print variables
+        //               and parametric form
+
+        if(matrix.isConsistent){
+            if(matrix.IsLinearlyIndependent()){
+                String topDivisor = "-------------------------------";
+                System.out.printf("\n|%-31s-%-31s-%-31s|\n", topDivisor, topDivisor, topDivisor, topDivisor);
+                System.out.printf("|%-31s|%-31s|%-31s|\n", "It is Consistent", "It is Linearly Independent", "It is Linearly Dependent");
+                System.out.printf("|%-31s|%-31s|%-31s|\n", topDivisor, topDivisor, topDivisor, topDivisor);
+                System.out.printf("|%-31s|%-31s|%-31s|\n", "YES", "YES", "NO");
+                System.out.printf("|%-31s-%-31s-%-31s|\n", topDivisor, topDivisor, topDivisor, topDivisor);
             }else{
-                PrintVariablesValues(matrix, amountPivots, freeVariables_Pivots);
-                PrintFreVariables(matrix, amountPivots, freeVariables_Pivots);
+                String topDivisor = "---------------------------";
+                System.out.printf("\n|%-31s-%-31s-%-31s|\n", topDivisor, topDivisor, topDivisor, topDivisor);
+                System.out.printf("|%-31s|%-31s|%-31s|\n", "It is Consistent", "It is Linearly Independent", "It is Linearly Dependent");
+                System.out.printf("|%-31s|%-31s|%-31s|\n", topDivisor, topDivisor, topDivisor, topDivisor);
+                System.out.printf("|%-31s|%-31s|%-31s|\n", "YES", "NO", "YES");
+                System.out.printf("|%-31s-%-31s-%-31s|\n", topDivisor, topDivisor, topDivisor, topDivisor);
             }
+            matrix.printVariablesValues();
+            matrix.printVariables_ParametricForm();
         }
-
-
-    }
-
-    private int[] ListPivots_AreFreeVariables(double[][] matrix, Dictionary variablesValues) {
-        int[] listOfFreeVariables = new int[0];
-        for (int indexColumn = 0; indexColumn < matrix[0].length -1; indexColumn++) {
-            double[] column = matrixMethods.GetColumnFromMatrix(matrix, indexColumn);
-            boolean isZero = matrixMethods.Array_isZero(column);
-            if (isZero) {
-                listOfFreeVariables = matrixMethods.Vector_AddElement(listOfFreeVariables, indexColumn);
-            }
-        }
-        if(listOfFreeVariables != null && listOfFreeVariables.length >= 1){
-            return listOfFreeVariables;
-        }else{
-            listOfFreeVariables = new int[0];
-            for (int index = 0; index < (variablesValues.Size()); index++) {
-                double[] zeroRow = new double[matrix[0].length];
-                boolean areEqual = java.util.Arrays.equals(matrix[index], zeroRow);
-                if(areEqual){
-                    listOfFreeVariables = matrixMethods.Vector_AddElement(listOfFreeVariables, index);
-                }
-            }
-            return listOfFreeVariables;
+        else{            
+            String topDivisor = "---------------------------";
+            System.out.printf("\n|%-30s-%-31s-%-31s|\n", topDivisor, topDivisor, topDivisor, topDivisor);
+            System.out.printf("|%-30s|%-31s|%-31s|\n", "It is Consistent", "It is Linearly Independent", "It is Linearly Dependent");
+            System.out.printf("|%-30s|%-31s|%-31s|\n", topDivisor, topDivisor, topDivisor, topDivisor);
+            System.out.printf("|%-30s|%-31s|%-31s|\n", "NO", "NA", "NA");
+            System.out.printf("|%-30s-%-31s-%-31s|\n", topDivisor, topDivisor, topDivisor, topDivisor);
         }
     }
 
-    private void PrintFreVariables(double[][] matrix, int amountPivots, int[] freeVariables_Pivots) {
-        int amountFreeVariables = matrix[0].length - 1 - amountPivots;
-        if(amountFreeVariables > 0){
-            System.out.println("The follow list shows the Free Variables");
-            System.out.print("{ ");
-            for (int variable = 0; variable < amountFreeVariables; variable++) {
-                if(freeVariables_Pivots != null && freeVariables_Pivots.length >= 1
-                    && freeVariables_Pivots.length == (variable + 1)){
-                        System.out.print("X"+ ( freeVariables_Pivots[variable] + 1 )+ ", ");
-                }else{
-                    System.out.print("X"+ ( amountPivots + variable + 1 ) + ", ");
-                }
-            }
-            System.out.print("}\n");
-        }
+    private void PrepareMatrix(){
+        matrix.DeleteRepetedRows();
+        matrix.TryGetPivots();
+        matrix.PrintPivots();
     }
 
-    private int CheckConsistency(double[][] matrix) {
-        int inconsistentIndex = -1;  
-        int counterRows = 0;      
-        for (double[] row : matrix) {
-            boolean rowFinished = false;
-            int index = -1;
-            int counterColumns = 0;
-            while (!rowFinished) {
-                if (row[counterColumns] != 0) {
-                    index = counterColumns;
-                    rowFinished = true;
-                }
-                if(counterColumns == (row.length -1 )){
-                    rowFinished = true;
-                }
-                counterColumns++;
-            }
-            if(index == ( row.length - 1 )){
-                inconsistentIndex = counterRows;
-            }
-            counterRows++;
-        }
-        return inconsistentIndex;
-    }
-
-    private void PrintVariablesValues(double[][] matrix, int amountPivots, int[] freeVariables_Pivots){
-        //Its going to look for a pivot in each column < amountPivots
-        //Every column is either zero, or has a unique non-zero value that is its pivot
-        System.out.println("The value of the variables are:");
-        int[] pivotIndices = matrixMethods.Matrix_GetPivotIndices_RandomTries(matrix);
-        if(pivotIndices != null & pivotIndices.length >= 1){
-            int counterVAriable = 1;
-            int indexColumn = 0;
-            for (int pivotIndex : pivotIndices) {
-                if(pivotIndex >= 0 ){
-                    System.out.print("X" + ( indexColumn + 1) + " : ");
-                    System.out.print(matrix[indexColumn][matrix[0].length - 1]);
-                    //this for goes from after the pivot  till second to last element ( < [].length -1)
-                    int counterFreeVar = 1;
-                    for (int freeVar = (amountPivots); freeVar < (matrix[0].length-1); freeVar++) {
-                        //the if checks if a pivot is a free variable. Other wise it start naming F.V with an index bigger than the amoun of pivots.
-                        if(freeVariables_Pivots != null && freeVariables_Pivots.length >= 1 && 
-                        freeVariables_Pivots.length == counterFreeVar){
-                            System.out.print(" + " + (-matrix[indexColumn][freeVar]) + "X" + (counterFreeVar+1));
-                        }else{
-                            System.out.print(" + " + (-matrix[indexColumn][freeVar]) + "X" + (freeVar+1));
-                        }
-                        counterFreeVar++;
-                    }
-                    System.out.print("\n");
-
-                    //the counter does not increase if there is not a valid pivot
-                    counterVAriable++;
-                }
-                indexColumn++;
-            }
-        }
-        
-    }
-
-    private Dictionary GetVariablesValues(double[][] matrix, int amountPivots) {    
-        Dictionary variablesValues = new Dictionary();
-        for (int counter = 0; counter < amountPivots; counter++) {
-            double[] pair = new double[2];
-            pair[0]= matrix[counter][counter];
-            pair[1]= matrix[counter][matrix[0].length - 1];
-            variablesValues.AddElement(pair);
-        }
-        return variablesValues;
-    }
-
-    public double[][] Reduce_StepByStep(double[][] matrix){    
-        //first the matrix is reordered to best find the pivot values
-        double[][] orderedMatrix = reArragenMatrix(matrix);  
-        matrixMethods.PrintMatrix("The rows were rearanged to find the pivots:", orderedMatrix);   
-
-        //Now the program gets as many pivots as possible
-        int[] newlistPivots = matrixMethods.Matrix_GetPivotIndices_RandomTries(orderedMatrix);
-        matrixMethods.PrintArray(newlistPivots, "Following the column order, the pivots are in the rows:");
-
-        //
-        double[][] newMatrix = reduceMatrix_GaussJordan(orderedMatrix, newlistPivots);
-        matrixMethods.PrintMatrix("\n\nThe final matrix is:", newMatrix);
-
-        return newMatrix;
-
-    }
-
-    public double[][] reArragenMatrix(double[][] matrix){
-        //Gets the pivot of as many columns as posible
-        int[] zeroColumns = matrixMethods.Matrix_CheckForZeroColumn(matrix);
-        //Reorder the rows in the matrix so the pivots are located in the main diagonal.
-        int[] listPivots = matrixMethods.Matrix_GetPivotIndices_RandomTries(matrix);
-        double[][] orderedMatrix = matrixMethods.ReorderMatrixRows(matrix, listPivots);
-        if(zeroColumns != null && zeroColumns.length >= 0){
-            System.out.print("Existence of zero columns was detected\n");            
-        }else{
-        }
-        
-        return orderedMatrix;
-    }
-
-    public double[][] getValidMatrix(){
-        boolean isValid = false;
-        double[][] matrix = {};
-        while ( !isValid ) {
-            System.out.println("You have chosen to solve a matrix. \n First you need to define a matrix.");
-            matrix = matrixMethods.Matrix_GetNew("A");
-            System.out.println("You have defined the next matrix:");
-            matrixMethods.PrintMatrix("A", matrix);
-            System.out.println("Please confirm the Matrix by typing [Y]/[y], or press [N]/[n] to re-enter the Matrix's values");
-            isValid = validation.validateInput_YesNo();
-        }
-        return matrix;
-    }
-
-    public double[][] reduceMatrix_GaussJordan(double[][] matrix, int[] indecesPivots){
-        int indexColumn = 0;
-        double[][] newMatrix = matrixMethods.CopyMatrix(matrix);
-        for (int indexPivot : indecesPivots) {
-            if(indexPivot < 0){
-                System.out.println("\n~~~~~~~~~~~~~~Step "+(indexColumn+1)+"~~~~~~~~~~~~~~");   
-                System.out.println("This column has no pivot. No operation is made in this step");   
-            }else if(newMatrix[indexPivot][indexColumn] == 0 ){
-                System.out.println("\n~~~~~~~~~~~~~~Step "+(indexColumn+1)+"~~~~~~~~~~~~~~");   
-                System.out.println("Pivot position: ("+(indexPivot+1)+","+(indexColumn+1)+")"); 
-                System.out.println("Pivot value: ("+newMatrix[indexPivot][indexColumn]+")"); 
-                System.out.println("This pivot value is 0, thus no operation is done in this step");  
+    private void Reduce_GaussJordan(){
+        System.out.println("======================== Solution step by step ========================");
+        for (int indexVar = 0; indexVar < matrix.pivots.size; indexVar++) {            
+            System.out.println("~~~~~~~~~~~~~~~ Step " +(indexVar + 1) +  " ~~~~~~~~~~~~~~~");
+            if(!matrix.pivots.areFree[indexVar]){
+                ReduceColumn(indexVar);
             }else{
-                System.out.println("\n~~~~~~~~~~~~~~Step "+(indexColumn+1)+"~~~~~~~~~~~~~~");   
-                double[] unitarianRow = matrixMethods.getUnitarianRow(newMatrix, indexColumn, indexPivot);
-                System.out.println("Pivot position: ("+(indexPivot+1)+","+(indexColumn+1)+")"); 
-                System.out.println("Pivot value: ("+newMatrix[indexPivot][indexColumn]+")"); 
-                System.out.println("Then, divinding row "+(indexPivot+1)+" by "+newMatrix[indexPivot][indexColumn]); 
-                matrixMethods.PrintArray(unitarianRow, "results in the new unitarian row:");
-                newMatrix = matrix_ClearColumnWithPivot(newMatrix, unitarianRow, unitarianRow[indexColumn], indexColumn, indexPivot);
-                newMatrix[indexPivot] = unitarianRow;
-                matrixMethods.PrintMatrix("\nResults in:", newMatrix);
+                System.out.println("Pivot "+(indexVar + 1) + " is a free variable. Hence, there is nothing to do in this step. \n");
             }
-            indexColumn++;
         }
-        return newMatrix;
     }
 
-    private double[][] matrix_ClearColumnWithPivot(double[][] matrix, double[] unitarianRow, double pivotValue, int indexColumn, int indexPivot) {
-        double[][] newMatrix = matrixMethods.CopyMatrix(matrix);
-        int counterRow = 0;
-        System.out.println(" "); 
-        for (double[] row : newMatrix) {
-            double negativeScalar = -1*newMatrix[counterRow][indexColumn];            
-            if(indexPivot != counterRow && negativeScalar !=0 && pivotValue != 0){
-                System.out.println("*Instruction: Multiplying the unitarian row by " + negativeScalar +" and adding it to row "+(counterRow+1)); 
-                newMatrix = AddRowsTogether(newMatrix, unitarianRow, counterRow, negativeScalar);
-            }
-            counterRow++;
+    private void ReduceColumn(int indexVar){
+        if(matrix.GetElement(matrix.pivots.positions[indexVar]) != 0){
+            String varName = "X"+ (indexVar + 1);
+            String position = "(" + (matrix.pivots.positions[indexVar][0]+1) + "," + (matrix.pivots.positions[indexVar][1]+1) + ")";
+            double value = matrix.GetElement(matrix.pivots.positions[indexVar]);
+
+            System.out.println("Pivot "+ varName + ",    Position = " + position+ ",   Value = " + value);
+            matrix.usePivotTo_ClearColumn(varName);
+            System.out.println("\n");
+        }else{
+            System.out.println("Pivot "+(indexVar + 1) + " has turn into a zero, so no operation is executed in this step \n");
         }
-        return newMatrix;
     }
 
-    public double[][] AddRowsTogether(double[][] matrix, double[] unitarianRow, int index_RowToModify, double negativeScalar ){
-        double[] unitarianRow_Modified = matrixMethods.vectorByScalar(unitarianRow, negativeScalar);
-        matrix[index_RowToModify] = matrixMethods.Vector_addTogether(unitarianRow_Modified, matrix[index_RowToModify]);
-        return matrix;
+    public void CreateNewMatrix(){
+        int AmountRows = -1;
+        while(AmountRows < 2){
+            System.out.println("Please indicate the amount of rows in the matrix");
+            AmountRows = validation.validateInput_isInt();
+        }
+        int AmountColumns = -1;
+        while(AmountColumns < 2){
+            System.out.println("Please indicate the amount of columns in the matrix");
+            AmountColumns = validation.validateInput_isInt();
+        }
+        int[] size = {AmountRows, AmountColumns};
+        matrix.SetSize(size);
+        matrix.PrintMatrix("\nThe matrix input is:\n");
+        matrix.setMatrix_Inputs();
+        matrix.PrintMatrix("\nThe matrix input is:");
     }
-
     
 }
